a2c97c2d9a1d4524984116a211270b7a28193181
Samyak Shah <samyakshah3008@gmail.com>
2024-07-27 02:02:14 +0530

    feat: super focus components and layout setup done

--- src/components/SuperFocus/SuperFocus.tsx
+++ src/components/SuperFocus/SuperFocus.tsx
@@ -1,5 +1,34 @@
+"use client";
+
+import { useState } from "react";
+import TimerWidget from "./TimerWidget";
+
 const SuperFocus = () => {
-  return <div>Super focus tomato timer page</div>;
+  const [activeState, setActiveState] = useState("Pomodoro");
+  const [startNewSession, setStartNewSession] = useState(false);
+
+  const getClassNameBasedOnState = () => {
+    if (activeState == "Pomodoro") {
+      return "bg-gradient-to-r from-red-100 to-red-600";
+    } else if (activeState == "Short break") {
+      return "bg-gradient-to-r from-yellow-100 to-yellow-600";
+    } else if (activeState == "Long break") {
+      return "bg-gradient-to-r from-green-100 to-green-600";
+    }
+  };
+
+  return (
+    <div style={{ height: "100vh" }} className={getClassNameBasedOnState()}>
+      <div>Navbar in construction</div>
+      <TimerWidget
+        activeState={activeState}
+        setActiveState={setActiveState}
+        startNewSession={startNewSession}
+        setStartNewSession={setStartNewSession}
+      />
+      <div>Tasks panel</div>
+    </div>
+  );
 };
 
 export default SuperFocus;
