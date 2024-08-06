eb25b62ee8b1b9a39d6d5a9d61a91b58ade7a2ee
Samyak Shah <samyakshah3008@gmail.com>
2024-08-06 18:28:12 +0530

    feat: guest login confirmation dialog add

---
+++ src/components/(unauthenticated)/guest-login-confirm-dialog.tsx
@@ -0,0 +1,88 @@
+import { saveAccessAndRefreshToken } from "@/lib/localstorage";
+import { signUpGuestUserService } from "@/services/authentication/signup";
+import { Loader } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Button } from "../ui/primitives/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/primitives/dialog";
+
+type GuestLoginConfirmDialogProps = {
+  isGuestLoginDialogOpen: boolean;
+  onCloseGuestLoginDialog: () => void;
+  onSignUpFlow: boolean;
+};
+
+const GuestLoginConfirmDialog = ({
+  isGuestLoginDialogOpen,
+  onCloseGuestLoginDialog,
+  onSignUpFlow = false,
+}: GuestLoginConfirmDialogProps) => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const router = useRouter();
+
+  const onConfirmGuestLogin = async () => {
+    try {
+      setIsSubmitting(true);
+      const {
+        data: { accessToken, refreshToken },
+      } = await signUpGuestUserService();
+      saveAccessAndRefreshToken(accessToken, refreshToken);
+      router.push("/dashboard");
+    } catch (error) {
+      console.error(error, "logged error");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const onCreateNewAccount = () => {
+    router.push("/signup");
+  };
+
+  return (
+    <Dialog
+      open={isGuestLoginDialogOpen}
+      onOpenChange={onCloseGuestLoginDialog}
+    >
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Confirm Guest Login</DialogTitle>
+        </DialogHeader>
+
+        <div>
+          You are about to Login as Guest, please note that being a guest you
+          won't be getting full access like for a verified account.
+        </div>
+
+        <DialogFooter className="mt-4">
+          <Button
+            onClick={
+              onSignUpFlow ? onCloseGuestLoginDialog : onCreateNewAccount
+            }
+          >
+            Create New Account
+          </Button>
+          <Button
+            variant="destructive"
+            disabled={isSubmitting}
+            onClick={onConfirmGuestLogin}
+          >
+            {isSubmitting ? (
+              <Loader className="mr-2 h-4 w-4 animate-spin" />
+            ) : null}
+            {isSubmitting ? "Setting up your account" : "Confirm"}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default GuestLoginConfirmDialog;
