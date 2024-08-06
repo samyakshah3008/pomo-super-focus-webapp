7d6bdc012f0c53efc760df01cae5e310a170ffdc
Samyak Shah <samyakshah3008@gmail.com>
2024-08-06 18:27:20 +0530

    enhancement: design updates

--- src/app/(unauthenticated)/signin/page.tsx
+++ src/app/(unauthenticated)/signin/page.tsx
@@ -18,27 +18,25 @@ export default async function SignInPage({}) {
   }
 
   return (
-    <>
-      <div className="w-screen max-w-lg px-4">
-        <div className="border-border dark:bg-background z-10 rounded-xl border bg-neutral-100 p-6">
-          <h1 className="text-2xl font-semibold">Sign in to your account</h1>
+    <div className="w-[512px] h-[420px] px-4">
+      <div className="border-border dark:bg-background z-10 rounded-xl border bg-neutral-100 p-6">
+        <h1 className="text-2xl font-semibold">Sign in to your account</h1>
 
-          <p className="text-muted-foreground mt-2 text-sm">
-            Welcome back, we are lucky to have you.
-          </p>
-          <hr className="-mx-6 my-4" />
-          <SignInForm />
-          <p className="text-muted-foreground mt-6 text-center text-sm">
-            Don't have an account?{" "}
-            <Link
-              href="/signup"
-              className="text-documenso-700 duration-200 hover:opacity-70"
-            >
-              Sign up
-            </Link>
-          </p>
-        </div>
+        <p className="text-muted-foreground mt-2 text-sm">
+          Welcome back Chief, we are lucky to have you.
+        </p>
+        <hr className="-mx-6 my-4" />
+        <SignInForm />
+        <p className="text-muted-foreground mt-6 text-center text-sm">
+          Don't have an account?{" "}
+          <Link
+            href="/signup"
+            className="text-documenso-700 duration-200 hover:opacity-70"
+          >
+            Sign up
+          </Link>
+        </p>
       </div>
-    </>
+    </div>
   );
 }
