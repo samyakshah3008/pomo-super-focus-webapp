4a52c76e0a500b04af3546087d410f5285ea37dd
Samyak Shah <samyakshah3008@gmail.com>
2024-08-12 18:01:52 +0530

    feat: store provider initial fetch calls add

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { fetchDailyProgress } from "@/lib/store/features/daily-progress/dailyProgressSlice";
+import { fetchStreakDetails } from "@/lib/store/features/streaks/streakSlice";
+import { fetchUserData } from "@/lib/store/features/user/userSlice";
 import { AppStore, makeStore } from "@/lib/store/store";
 import { useRef } from "react";
 import { Provider } from "react-redux";
@@ -8,8 +11,12 @@ const StoreProvider = ({ children }: { children: React.ReactNode }) => {
   const storeRef = useRef<AppStore | null>(null);
   if (!storeRef.current) {
     storeRef.current = makeStore();
-    // add initial state
-    // storeRef.current.dispatch();
+
+    Promise.allSettled([
+      storeRef.current.dispatch(fetchUserData()),
+      storeRef.current.dispatch(fetchDailyProgress()),
+      storeRef.current.dispatch(fetchStreakDetails()),
+    ]);
   }
 
   return <Provider store={storeRef.current}>{children}</Provider>;
