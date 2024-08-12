6164605a37dbbed0f885a13a605cd313268a2bef
Samyak Shah <samyakshah3008@gmail.com>
2024-08-12 18:04:40 +0530

    feat: daily progress widget component add

--- src/app/super-focus/@dailyProgress/daily-progress-widget.tsx
+++ src/app/super-focus/@dailyProgress/daily-progress-widget.tsx
@@ -1,62 +1,105 @@
 "use client";
 
-import { userId } from "@/constants/user";
-import {
-  getDailyProgressStatsService,
-  getStreakDetailsService,
-} from "@/services/daily-progress/daily-progress";
-import { useEffect, useState } from "react";
-import EditDailyGoalModal from "./edit-daily-goal-modal";
+import ProgressWrapper from "@/components/(super-focus)/progress-wrapper";
+import { useToast } from "@/components/ui/primitives/use-toast";
+import { getProgressPercentage } from "@/utils/helper-functions";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import EditGoalButton from "./edit-goal-btn";
 
 const DailyProgressWidget = () => {
-  const [dailyProgressStats, setDailyProgressStats] = useState(null);
-  const [streakDetailsObj, setStreakDetailsObj] = useState({});
+  const dailyProgressDetails = useSelector(
+    (state: any) => state?.dailyProgress
+  );
+  const streakDetails = useSelector((state: any) => state?.streakDetails);
+  const { toast } = useToast();
 
-  const getDailyProgressStats = async () => {
-    const queryParam = {
-      userId,
-    };
-    try {
-      const response = await getDailyProgressStatsService(queryParam);
-      setDailyProgressStats(response?.data?.data);
-    } catch (error) {
-      console.log(error, "error while getting daily progress stats. ");
+  const checkForErrors = () => {
+    if (dailyProgressDetails.status == "rejected") {
+      toast({
+        variant: "destructive",
+        title: "Failed to fetch daily progress",
+        description:
+          "Something went wrong with our servers, we are sorry about it. Please try again later.",
+      });
     }
-  };
 
-  const getStreakDetails = async () => {
-    const queryParam = {
-      userId,
-      currentDate: new Date().toISOString(),
-    };
-    try {
-      const response = await getStreakDetailsService(queryParam);
-      const streakDetails = response?.data?.data?.streakDetails;
-      setStreakDetailsObj(streakDetails);
-    } catch (error) {
-      console.log(error);
+    if (streakDetails.status == "rejected") {
+      toast({
+        variant: "destructive",
+        title: "Failed to fetch streak details",
+        description:
+          "Something went wrong with our servers, we are sorry about it. Please try again later.",
+      });
     }
   };
 
   useEffect(() => {
-    getDailyProgressStats();
-    getStreakDetails();
-  }, []);
+    if (
+      dailyProgressDetails?.status == "pending" ||
+      streakDetails?.status == "pending"
+    )
+      return;
+    checkForErrors();
+  }, [dailyProgressDetails, streakDetails]);
 
-  if (!dailyProgressStats) return;
+  if (
+    dailyProgressDetails?.status == "pending" ||
+    streakDetails?.status == "pending"
+  ) {
+    return <div>Loading...</div>;
+  }
+
+  const progressPercentage = getProgressPercentage(
+    streakDetails?.dailyGoalInHours,
+    dailyProgressDetails?.today?.totalTime
+  );
 
   return (
-    <div>
-      Daily Progress Component
-      <div>
-        Yesterday's Focus Time: {dailyProgressStats?.yesterday?.totalTime}{" "}
+    <div className="flex flex-col gap-5 p-4 w-[500px]">
+      <div className="flex justify-between items-center">
+        <div className="text-base font-medium">Daily Progress</div>
+        <EditGoalButton
+          currentDailyGoal={streakDetails?.streakDetails?.dailyGoalInHours}
+        />
       </div>
-      <div>
-        Today's total focus time so far: {dailyProgressStats?.today?.totalTime}{" "}
+
+      <div className="flex items-center justify-around">
+        <div className="flex flex-col gap-2 items-center">
+          <div className="text-sm text-muted-foreground">Yesterday</div>
+          <div className="text-3xl font-bold tracking-tighter">
+            {dailyProgressDetails?.dailyProgressDetails?.yesterday?.totalTime}
+          </div>
+          <div className="text-sm text-muted-foreground">minutes</div>
+        </div>
+
+        <div className="flex flex-col gap-2 items-center">
+          <ProgressWrapper progress={progressPercentage}>
+            <div className="text-sm text-muted-foreground">Daily goal</div>
+            <div className="text-3xl font-bold tracking-tighter">
+              {streakDetails?.streakDetails?.dailyGoalInHours}
+            </div>
+            <div className="text-sm text-muted-foreground">
+              {streakDetails?.streakDetails?.dailyGoalInHours < 1.5
+                ? "hour"
+                : "hours"}
+            </div>
+          </ProgressWrapper>
+          <div className="text-sm text-muted-foreground">
+            Completed:{" "}
+            {dailyProgressDetails?.dailyProgressDetails?.today?.totalTime}{" "}
+            minutes
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2 items-center">
+          <div className="text-sm text-muted-foreground">Streak</div>
+          <div className="text-3xl font-bold tracking-tighter">
+            {streakDetails?.streakDetails?.streakCount}
+          </div>
+          <div className="text-sm text-muted-foreground"> days</div>
+        </div>
       </div>
-      <div>Streak: {streakDetailsObj?.streakCount} </div>
-      <div>Highest Streak: {streakDetailsObj?.highestStreak}</div>
-      <EditDailyGoalModal />
     </div>
   );
 };
