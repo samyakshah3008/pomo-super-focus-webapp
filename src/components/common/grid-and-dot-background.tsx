aac4de30b4df1884fe466f310d0de48703175f6e
Samyak Shah <samyakshah3008@gmail.com>
2024-10-01 17:42:54 +0530

    feat: grid and dot background layout ui component add

---
+++ src/components/common/grid-and-dot-background.tsx
@@ -0,0 +1,32 @@
+export function GridBackground({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="h-screen w-full dark:bg-black bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      {children}
+    </div>
+  );
+}
+
+export function GridSmallBackground({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex justify-center">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      {children}
+    </div>
+  );
+}
+
+export function DotBackground({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex justify-center">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      {children}
+    </div>
+  );
+}
