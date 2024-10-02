844dd4f84fada8bee87fc7a823bc2084b3523b90
Samyak Shah <samyakshah3008@gmail.com>
2024-10-02 17:27:29 +0530

    feat: dynamic values add for progress component

--- src/components/common/progress.tsx
+++ src/components/common/progress.tsx
@@ -7,12 +7,18 @@ import { Progress } from "@/components/ui/primitives/progress";
 export function ProgressBar({
   progress,
   setProgress,
+  fetchedChecklistCompletedCount,
+  totalCount,
 }: {
   progress: any;
   setProgress: any;
+  fetchedChecklistCompletedCount: any;
+  totalCount: number;
 }) {
+  let percentage = (fetchedChecklistCompletedCount * 100) / totalCount;
+
   useEffect(() => {
-    const timer = setTimeout(() => setProgress(50), 500);
+    const timer = setTimeout(() => setProgress(percentage), 500);
     return () => clearTimeout(timer);
   }, []);
 
