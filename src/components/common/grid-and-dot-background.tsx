dadbe618832b9c87c9390d103ae289cf7ee92560
Samyak Shah <samyakshah3008@gmail.com>
2024-10-02 18:05:45 +0530

    fix: ui bug resolve

--- src/components/common/grid-and-dot-background.tsx
+++ src/components/common/grid-and-dot-background.tsx
@@ -23,7 +23,7 @@ export function GridSmallBackground({
 
 export function DotBackground({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex justify-center">
+    <div className="w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex justify-center">
       {/* Radial gradient for the container to give a faded look */}
       {/* <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div> */}
       {children}
