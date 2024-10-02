01fe4292256938e087078ec20b761982b5e704e3
Samyak Shah <samyakshah3008@gmail.com>
2024-10-02 12:51:47 +0530

    feat: complete dashboard screen UI

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,15 @@
+import GreetingModal from "@/components/(dashboard)/greeting-modal";
+import { DotBackground } from "@/components/common/grid-and-dot-background";
+import { Separator } from "@/components/ui/primitives/separator";
 import { accessTokenKeyBrowserStorage } from "@/constants/browser-storage";
 import { getCookie } from "cookies-next";
 import { Metadata } from "next";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import Checklist from "./checklist";
+import Header from "./header";
+import ProgressReview from "./progress-review";
+import QuickStart from "./quick-start";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -14,5 +21,29 @@ export default async function DashboardPage({}) {
   if (!accessToken) {
     redirect("/signin");
   }
-  return <div>Dashboard</div>;
+
+  const sections = [
+    <Header />,
+    <Checklist />,
+    <QuickStart />,
+    <ProgressReview />,
+  ];
+
+  return (
+    <div>
+      <GreetingModal />
+      <DotBackground>
+        <div className="flex flex-col w-[80%] gap-10 ">
+          {sections.map((section, id) => {
+            return (
+              <>
+                {section}
+                {sections?.length !== id ? <Separator /> : null}
+              </>
+            );
+          })}
+        </div>
+      </DotBackground>
+    </div>
+  );
 }
