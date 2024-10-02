3a76f1ee33053353db4e5d380de69fd4743a10e4
Samyak Shah <samyakshah3008@gmail.com>
2024-10-02 18:45:34 +0530

    feat: dynamic values add for content

--- src/app/dashboard/header.tsx
+++ src/app/dashboard/header.tsx
@@ -1,22 +1,45 @@
 "use client";
+
 import Lottie from "lottie-react";
 import DashboardWitch from "../../../public/witch-dashboard.json";
 
+import {
+  getCurrentYear,
+  getDaysUntilYearEnd,
+  getTimeOfDay,
+} from "@/utils/helper-functions";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { TypewriterEffectSmooth } from "../../components/ui/typewriter-effect";
 
+type Word = {
+  text: string;
+  className?: string;
+};
+
 const Header = () => {
-  const words = [
-    {
-      text: "Good",
-    },
-    {
-      text: "Morning,",
-    },
-    {
-      text: "Samyak Shah!",
-      className: "text-blue-500 dark:text-blue-500",
-    },
-  ];
+  const currentUser = useSelector((state: any) => state?.user);
+  const [words, setWords] = useState<Word[]>([]);
+
+  useEffect(() => {
+    if (!currentUser?.pomoSuperUser) return;
+    setWords([
+      {
+        text: "Good",
+      },
+      {
+        text: getTimeOfDay(),
+      },
+      {
+        text: `${currentUser?.pomoSuperUser?.firstName}`,
+        className: "text-blue-500 dark:text-blue-500",
+      },
+    ]);
+  }, [currentUser?.pomoSuperUser]);
+
+  if (!currentUser?.pomoSuperUser || !words?.length) {
+    return null;
+  }
 
   return (
     <div className="flex flex-col items-center">
@@ -30,8 +53,9 @@ const Header = () => {
       </div>
       <TypewriterEffectSmooth words={words} />
       <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
-        Each new fresh day is an opportunity, you have 99 fresh opportunities
-        till 2024 ends. Make the best out of it! 👀
+        Each new fresh day is an opportunity, you have {getDaysUntilYearEnd()}{" "}
+        fresh opportunities till {getCurrentYear()} ends. Make the best out of
+        it! 👀
       </p>
     </div>
   );
