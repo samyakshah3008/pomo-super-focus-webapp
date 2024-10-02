2262b05f75a6898ed4c5334bd89fbf391db3a149
Samyak Shah <samyakshah3008@gmail.com>
2024-10-02 12:56:54 +0530

    feat: progress review section for dashboard add

---
+++ src/app/dashboard/progress-review.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Lottie from "lottie-react";
+import DashboardProgressReview from "../../../public/progress-dashboard.json";
+import DailyProgressWidget from "../super-focus/@dailyProgress/daily-progress-widget";
+
+const ProgressReview = () => {
+  return (
+    <div className="flex flex-col gap-5 align-center justify-center">
+      <div className="text-2xl font-bold text-center">
+        Quick look to your efforts!
+      </div>
+      <div className="flex items-center justify-center gap-10">
+        <div className="w-[250px]">
+          <Lottie
+            animationData={DashboardProgressReview}
+            loop={true}
+            width={250}
+          />
+        </div>
+        <DailyProgressWidget />
+      </div>
+    </div>
+  );
+};
+
+export default ProgressReview;
