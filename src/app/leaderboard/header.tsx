7489d50505d543d0d55605c679cfb24675272f02
Samyak Shah <samyakshah3008@gmail.com>
2024-10-03 18:00:01 +0530

    feat: leaderboard various sections ui screens complete with api integration

---
+++ src/app/leaderboard/header.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { getCurrentYear, getDaysUntilYearEnd } from "@/utils/helper-functions";
+import Image from "next/image";
+import WitchyLeaders from "../../../public/witch-leaders.png";
+
+const Header = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="mt-10 mb-10">
+        <Image
+          width={300}
+          height={300}
+          src={WitchyLeaders}
+          alt="witchy-leaders"
+        />
+      </div>
+      <div className="text-xs sm:text-base md:text-xl lg:text:3xl xl:text-5xl font-bold">
+        Meet our <span className="text-blue-500">PomoSuperLeaders!</span>{" "}
+      </div>
+      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base text-center mt-3 ">
+        Let’s take a moment to appreciate the hard work of our PomoSuperLeaders
+        who continue to push their limits. Check your rank, and use it as
+        motivation to climb even higher and make the most of the remaining{" "}
+        {getDaysUntilYearEnd()} days of {getCurrentYear()}!
+      </p>
+    </div>
+  );
+};
+
+export default Header;
