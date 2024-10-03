df69dd00080f13640c54f58d14960ae640a524d6
Samyak Shah <samyakshah3008@gmail.com>
2024-10-03 18:00:49 +0530

    feat: leaderboard screen complete

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -2,17 +2,12 @@ import { accessTokenKeyBrowserStorage } from "@/constants/browser-storage";
 import { getCookie } from "cookies-next";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import LeaderboardData from "./leaderboard-data";
+import LeaderboardLayout from "./leaderboard-layout";
 
 export default async function LeaderBoardPage() {
   const accessToken = getCookie(accessTokenKeyBrowserStorage, { cookies });
   if (!accessToken) {
     redirect("/signin");
   }
-  return (
-    <div>
-      Here Leaderboard page is under construction
-      <LeaderboardData />
-    </div>
-  );
+  return <LeaderboardLayout />;
 }
