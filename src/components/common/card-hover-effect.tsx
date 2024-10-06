579098acbe0de246ba151bea75d9914d5a99196e
Samyak Shah <samyakshah3008@gmail.com>
2024-10-06 16:59:37 +0530

    improvements: add more props according to usecase

--- src/components/common/card-hover-effect.tsx
+++ src/components/common/card-hover-effect.tsx
@@ -1,9 +1,9 @@
 import { HoverEffect } from "../ui/card-hover-effect";
 
-const CardHoverEffect = ({ cardItems }: { cardItems: any }) => {
+const CardHoverEffect = ({ cardItems, type }: any) => {
   return (
     <div className="max-w-5xl mx-auto px-8">
-      <HoverEffect items={cardItems} />
+      <HoverEffect items={cardItems} type={type} />
     </div>
   );
 };
