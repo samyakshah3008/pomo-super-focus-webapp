5db80b25a5a362fe5f1ad43f03e2d1dd3de765c7
Samyak Shah <samyakshah3008@gmail.com>
2024-10-09 01:02:14 +0530

    chore: constants for habits scorecard flow

---
+++ src/app/habits-scorecard/constants.ts
@@ -0,0 +1,12 @@
+const habitCategories = [
+  "Fitness",
+  "Health",
+  "Work",
+  "Education",
+  "Personal Development",
+  "Hobbies",
+  "Finance",
+  "Social",
+];
+
+export { habitCategories };
