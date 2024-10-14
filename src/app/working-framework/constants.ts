b8c178774b378607ff8d03194fa3e95efee5153c
Samyak Shah <samyakshah3008@gmail.com>
2024-10-14 22:00:48 +0530

    feat: constants for working framework flow add

---
+++ src/app/working-framework/constants.ts
@@ -0,0 +1,10 @@
+const buildCustomFrameworkCard = [
+  {
+    title: "Craft your Custom Framework!",
+    description:
+      "Everyone have their own productivity formula, rediscover your best formula by creating your framework and follow it. You can always come back and change it.",
+    link: "/working-framework/create",
+  },
+];
+
+export { buildCustomFrameworkCard };
