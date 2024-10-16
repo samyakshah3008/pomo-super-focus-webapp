c22f87a454a279ea05395ccc4211712085f37811
Samyak Shah <samyakshah3008@gmail.com>
2024-10-16 18:19:07 +0530

    constants file add for super focus screen

---
+++ src/app/super-focus/constants.ts
@@ -0,0 +1,28 @@
+const pomoSuperFocusTimerFeatures = [
+  {
+    icon: "⚡",
+    label: "Configure time, auto-start, music, intervals",
+  },
+  {
+    icon: "⚡",
+    label: "Syncs music with timer start / stop",
+  },
+  {
+    icon: "⚡",
+    label: "Plays nature ambiance during the break",
+  },
+  {
+    icon: "⚡",
+    label: "Built in task manager removes distractions",
+  },
+  {
+    icon: "⚡",
+    label: "Ad free and free to use",
+  },
+  {
+    icon: "⚡",
+    label: "Settings save to your browser",
+  },
+];
+
+export { pomoSuperFocusTimerFeatures };
