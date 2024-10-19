e4e607a4f8186b32e7b43bfb982641dc8405c553
Samyak Shah <samyakshah3008@gmail.com>
2024-10-20 01:16:28 +0530

    feat: music widget ui screen add

--- src/app/super-focus/music-widget.tsx
+++ src/app/super-focus/music-widget.tsx
@@ -1,5 +1,37 @@
-const MusicWidget = () => {
-  return <div>Music will come here</div>;
+const MusicWidget = ({
+  studySource,
+}: {
+  studySource: "spotify" | "youtube";
+}) => {
+  return (
+    <div className="">
+      {studySource == "spotify" ? (
+        <iframe
+          title="Spotify Playlist"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+          width="100%"
+          height="360"
+          src="https://open.spotify.com/embed/playlist/0vvXsWCC9xrXsKd4FyS8kM?utm_source=iframe-api"
+          className="rounded-3xl"
+        ></iframe>
+      ) : (
+        <iframe
+          id="player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          title="lofi hip hop radio - beats to study/relax to 🐾"
+          width="640"
+          height="320"
+          src="https://www.youtube.com/embed/7NOSDKb0HlU?playsinline=1&enablejsapi=1&origin=https%3A%2F%2Ftimer.flotes.app&widgetid=2"
+          allowFullScreen
+          className="rounded-3xl"
+        ></iframe>
+      )}
+    </div>
+  );
 };
 
 export default MusicWidget;
