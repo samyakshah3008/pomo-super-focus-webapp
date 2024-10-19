05be87857c6d65cd5ff6eef0741477c78f4ba910
Samyak Shah <samyakshah3008@gmail.com>
2024-10-20 01:28:58 +0530

    minor ui update

--- src/app/super-focus/tasks.tsx
+++ src/app/super-focus/tasks.tsx
@@ -1,14 +1,14 @@
-import Image from "next/image";
-import Underconstruction from "../../../public/under-construction.png";
+"use client";
+
+import Lottie from "lottie-react";
+import Underconstruction from "../../../public/under-construction-man.json";
 
 const Tasks = () => {
   return (
     <div className="w-[70%] m-auto flex flex-col items-center gap-5 justify-center">
-      <Image
-        src={Underconstruction}
-        alt="under-construction"
-        className="w-96 h-96"
-      />
+      <div className="w-96 h-96">
+        <Lottie animationData={Underconstruction} loop={true} />
+      </div>
       <div className="text-2xl font-bold text-center">
         This feature is under construction, we will update you once we launch
         this. Meanwhile, we appreciate your patience:)
