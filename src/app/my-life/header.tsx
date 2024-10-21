857355ddfd2278906a87a1ab13c08465ec515233
Samyak Shah <samyakshah3008@gmail.com>
2024-10-21 22:13:34 +0530

    feat: my life header section add

--- src/app/my-life/header.tsx
+++ src/app/my-life/header.tsx
@@ -1,5 +1,61 @@
-const Header = () => {
-  return <div>Header</div>;
+"use client";
+
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+import Lottie from "lottie-react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import HeartPulse from "../../../public/heart-pulse.json";
+
+type Word = {
+  text: string;
+  className?: string;
+};
+
+const Header = ({ lifeLeftObj }: any) => {
+  const currentUser = useSelector((state: any) => state?.user);
+  const [words, setWords] = useState<Word[]>([]);
+
+  useEffect(() => {
+    if (!currentUser?.pomoSuperUser || !lifeLeftObj?.totalWeeksLeft) return;
+    setWords([
+      {
+        text: `${currentUser?.pomoSuperUser?.firstName},`,
+        className: "text-blue-500 dark:text-blue-500",
+      },
+      {
+        text: "You",
+      },
+      {
+        text: `have`,
+      },
+      {
+        text: `only`,
+      },
+      {
+        text: `${lifeLeftObj?.totalWeeksLeft} weeks`,
+      },
+      {
+        text: `left. ⌛`,
+      },
+    ]);
+  }, [currentUser?.pomoSuperUser, lifeLeftObj]);
+
+  if (!currentUser?.pomoSuperUser || !words?.length) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="w-64 flex m-auto">
+        <Lottie animationData={HeartPulse} loop={true} />
+      </div>
+      <TypewriterEffectSmooth words={words} />
+      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base">
+        Your life is too precious, don't waste a single second towards
+        meaningless things. 👀
+      </p>
+    </div>
+  );
 };
 
 export default Header;
