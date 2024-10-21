b7905ed980da83ad8a3f440ac6aff1b31fadc4a6
Samyak Shah <samyakshah3008@gmail.com>
2024-10-21 22:15:51 +0530

    feat: date picker shadcn component add

---
+++ src/components/common/date-picker.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/primitives/button";
+import { Calendar } from "../ui/primitives/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "../ui/primitives/popover";
+
+function DatePicker({ date, setDate }: any) {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[280px] justify-start text-left font-normal",
+            !date && "text-muted-foreground"
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+export default DatePicker;
