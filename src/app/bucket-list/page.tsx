443ac55fb71dcf89625e2bc9739d70602962885d
Samyak Shah <samyakshah3008@gmail.com>
2024-10-22 23:07:19 +0530

    feat: bucket list route add

---
+++ src/app/bucket-list/page.tsx
@@ -0,0 +1,20 @@
+import { accessTokenKeyBrowserStorage } from "@/constants/browser-storage";
+import { getCookie } from "cookies-next";
+import { Metadata } from "next";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import BucketList from "./bucket-list";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Dashboard for Pomo Super Focus Users.",
+};
+
+export default async function DashboardPage({}) {
+  const accessToken = getCookie(accessTokenKeyBrowserStorage, { cookies });
+  if (!accessToken) {
+    redirect("/signin");
+  }
+
+  return <BucketList />;
+}
