d47c774b44433cd5b411980f25d7dda44b9d9d7f
Samyak Shah <samyakshah3008@gmail.com>
2024-10-22 23:06:08 +0530

    feat: bucket list component add

---
+++ src/app/bucket-list/bucket-list.tsx
@@ -0,0 +1,13 @@
+import Header from "./header";
+import MainContainer from "./main-container";
+
+const BucketList = () => {
+  return (
+    <div className="flex flex-col gap-4 items-center">
+      <Header />
+      <MainContainer />
+    </div>
+  );
+};
+
+export default BucketList;
