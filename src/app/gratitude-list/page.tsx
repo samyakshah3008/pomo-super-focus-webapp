8ca3c10bb3dc74a9261cb189241c1aa59326b8e7
Samyak Shah <samyakshah3008@gmail.com>
2024-10-22 23:09:53 +0530

    feat: gratitude list route wire

---
+++ src/app/gratitude-list/page.tsx
@@ -0,0 +1,20 @@
+import { accessTokenKeyBrowserStorage } from "@/constants/browser-storage";
+import { getCookie } from "cookies-next";
+import { Metadata } from "next";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import GratitudeList from "./gratitude-list";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Dashboard for Pomo Super Focus Users.",
+};
+
+export default async function DashboardPage({}) {
+  const accessToken = getCookie(accessTokenKeyBrowserStorage, { cookies });
+  if (!accessToken) {
+    redirect("/signin");
+  }
+
+  return <GratitudeList />;
+}
