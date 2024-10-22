1642a9adef07b076e4e5f92ebffb4bb517bb6935
Samyak Shah <samyakshah3008@gmail.com>
2024-10-22 23:06:30 +0530

    feat: header for bucket list add

---
+++ src/app/bucket-list/header.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Lottie from "lottie-react";
+import NightWitch from "../../../public/witch-3.json";
+
+const Header = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="w-64 m-auto">
+        <Lottie animationData={NightWitch} loop={true} />
+      </div>
+      <div className="text-xs sm:text-base md:text-xl lg:text:3xl xl:text-5xl font-bold">
+        Time to check off your{" "}
+        <span className="text-blue-500">Bucket List</span> one at a time!
+      </div>
+      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base text-center mt-3 w-[800px]">
+        Turning dreams into reality, one step at a time—whether in life or
+        career.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
