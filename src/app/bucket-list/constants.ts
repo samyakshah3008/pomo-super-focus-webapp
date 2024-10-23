a6b2ee603486d1b32497b2514892a174be540b01
Samyak Shah <samyakshah3008@gmail.com>
2024-10-24 02:11:51 +0530

    constants for bucket list and gratitude list add

---
+++ src/app/bucket-list/constants.ts
@@ -0,0 +1,36 @@
+import { BucketItem } from "@/components/(bucket-list)/data-table";
+
+const dummyBucketList: BucketItem[] = [
+  {
+    id: "1",
+    title: "Build a remote office",
+    description: "Lorem Epsum",
+    isCompleted: false,
+  },
+  {
+    id: "2",
+    title: "Retire parents",
+    description: "Lorem Epsum",
+    isCompleted: false,
+  },
+  {
+    id: "3",
+    title: "Earn first income!",
+    description: "Lorem Epsum",
+    isCompleted: true,
+  },
+  {
+    id: "4",
+    title: "Watch Royal Challengers Bangalore winning IPL Trophy alive.",
+    description: "Lorem Epsum",
+    isCompleted: false,
+  },
+  {
+    id: "5",
+    title: "Selfie with Virat Kohli!",
+    description: "Lorem Epsum",
+    isCompleted: false,
+  },
+];
+
+export { dummyBucketList };
---
+++ src/app/gratitude-list/constants.ts
@@ -0,0 +1,41 @@
+import { GratitudeItem } from "@/components/(gratitude-list)/data-table";
+
+const dummyGratitudeList: GratitudeItem[] = [
+  {
+    id: "1",
+    title: "Best health",
+    description:
+      "This is most underrated, but I want to remind this to myself. ",
+    date: "23rd October 2024",
+  },
+  {
+    id: "2",
+    title: "Good Quality of life",
+    description:
+      "This is most underrated, but I want to remind this to myself. ",
+    date: "23rd October 2024",
+  },
+  {
+    id: "3",
+    title: "Living with parents",
+    description:
+      "This is most underrated, but I want to remind this to myself. ",
+    date: "23rd October 2024",
+  },
+  {
+    id: "4",
+    title: "Born in India!",
+    description:
+      "This is most underrated, but I want to remind this to myself. ",
+    date: "23rd October 2024",
+  },
+  {
+    id: "5",
+    title: "Born in a jain family!",
+    description:
+      "This is most underrated, but I want to remind this to myself. ",
+    date: "23rd October 2024",
+  },
+];
+
+export { dummyGratitudeList };
