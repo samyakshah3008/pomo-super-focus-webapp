70308f9f4ac54748ce461d1b81300408523ac782
Samyak Shah <samyakshah3008@gmail.com>
2024-10-23 17:18:17 +0530

    feat: main container section add for goals

--- src/app/goals/main-container.tsx
+++ src/app/goals/main-container.tsx
@@ -1,5 +1,13 @@
+import { DataTable } from "@/components/(goals)/data-table";
+import { goalsDummyData } from "./constants";
+
 const MainContainer = () => {
-  return <div>Main container</div>;
+  let data = goalsDummyData;
+  return (
+    <div className="w-[80%]">
+      <DataTable data={data} />
+    </div>
+  );
 };
 
 export default MainContainer;
