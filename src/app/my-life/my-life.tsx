4adfeed6206e07b131696f79b56184d5d09e08c9
Samyak Shah <samyakshah3008@gmail.com>
2024-10-27 01:13:47 +0530

    feat: my life root component various cases and error handlings complete

--- src/app/my-life/my-life.tsx
+++ src/app/my-life/my-life.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import HowToModal from "@/components/common/how-to-modal";
-import Lottie from "lottie-react";
+import { useToast } from "@/components/ui/primitives/use-toast";
+import { IconBulbFilled } from "@tabler/icons-react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import Bulb from "../../../public/bulb.json";
 import { howToModalMyLifeObj } from "./constant";
 import Header from "./header";
 import { calculateLifeLeft } from "./helper";
@@ -13,16 +13,56 @@ import TimeLeftGrid from "./time-left-grid";
 import YearsGrid from "./years-grid";
 
 const MyLife = () => {
-  const [isOnboardingCompleted, setIsOnboardingCompleted] = useState(true);
+  const [isOnboardingCompleted, setIsOnboardingCompleted] = useState(false);
   const [lifeLeftObj, setLifeLeftObj] = useState<any>(null);
   const [showHowToModal, setShowHowToModal] = useState(false);
 
   const currentUser = useSelector((state: any) => state?.user);
 
+  const { toast } = useToast();
+
+  const showSuccessOnboarding = () => {
+    toast({
+      variant: "default",
+      title: "Welcome onboard!",
+      description: "We are pleased to have you here!",
+    });
+  };
+
   useEffect(() => {
-    if (!currentUser?.pomoSuperUser) return;
-    const timeLeft = calculateLifeLeft("30-08-2003", 60);
-    setLifeLeftObj(timeLeft);
+    if (!currentUser?.pomoSuperUser?._id) return;
+    if (currentUser?.pomoSuperUser?.isMyLifeOnboardingComplete) {
+      const { birthDate, estimateLifeSpan } = currentUser?.pomoSuperUser;
+      setIsOnboardingCompleted(true);
+      const timeLeft = calculateLifeLeft(birthDate, estimateLifeSpan);
+      setLifeLeftObj(timeLeft);
+    } else {
+      setIsOnboardingCompleted(false);
+    }
+  }, [currentUser]);
+
+  useEffect(() => {
+    if (
+      !currentUser?.pomoSuperUser?._id ||
+      !currentUser.pomoSuperUser.isMyLifeOnboardingComplete
+    )
+      return;
+
+    const { birthDate, estimateLifeSpan } = currentUser.pomoSuperUser;
+
+    const id = setInterval(() => {
+      const timeLeft = calculateLifeLeft(birthDate, estimateLifeSpan);
+      if (timeLeft) {
+        setLifeLeftObj(timeLeft);
+        if (timeLeft.isLifeSpanCompleted) {
+          clearInterval(id);
+        }
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(id);
+    };
   }, [currentUser]);
 
   if (!currentUser?.pomoSuperUser) {
@@ -30,7 +70,12 @@ const MyLife = () => {
   }
 
   if (!isOnboardingCompleted) {
-    return <Onboarding setIsOnboardingCompleted={setIsOnboardingCompleted} />;
+    return (
+      <Onboarding
+        showSuccessOnboarding={showSuccessOnboarding}
+        setIsOnboardingCompleted={setIsOnboardingCompleted}
+      />
+    );
   }
   return (
     <div className="flex flex-col gap-4 pl-10 pr-10 pb-5 w-[85%] m-auto ">
@@ -39,12 +84,9 @@ const MyLife = () => {
       <YearsGrid lifeLeftObj={lifeLeftObj} />
 
       <div className="absolute top-10 right-40">
-        {" "}
-        <Lottie
+        <IconBulbFilled
           onClick={() => setShowHowToModal(true)}
-          className="cursor-pointer w-24 h-24"
-          animationData={Bulb}
-          loop={true}
+          className="cursor-pointer text-yellow-400 w-20 h-20"
         />
       </div>
       <HowToModal
