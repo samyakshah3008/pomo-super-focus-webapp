c12e10df9da5d7021989947441dfd3443601bb89
Samyak Shah <samyakshah3008@gmail.com>
2024-10-30 11:37:05 +0530

    refactor - minor improvements

--- src/components/layouts/side-nav.tsx
+++ src/components/layouts/side-nav.tsx
@@ -12,6 +12,7 @@ import {
   IconHeart,
   IconHeartbeat,
   IconHome,
+  IconLogout2,
   IconSettingsAutomation,
   IconStairsUp,
   IconTableSpark,
@@ -19,6 +20,7 @@ import {
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
+import ConfirmLogoutModal from "../(settings)/confirm-logout-modal";
 import { Sidebar, SidebarBody, SidebarLink } from "../ui/sidebar";
 
 const SideNav = () => {
@@ -110,38 +112,64 @@ const SideNav = () => {
   ];
 
   const [open, setOpen] = useState(false);
+  const [showConfirmLogoutModal, setShowConfirmLogoutModal] = useState(false);
 
   return (
-    <div
-      className={cn(
-        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 max-w-7xl border border-neutral-200 dark:border-neutral-700 overflow-hidden",
-        "md:h-screen sm:h-8 md:sticky top-0 h-screen"
-      )}
-    >
-      <Sidebar open={open} setOpen={setOpen}>
-        <SidebarBody className="justify-between gap-10">
-          <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-            {open ? <Logo /> : <LogoIcon />}
-            <div className="mt-8 flex flex-col gap-2">
-              {navData.map((link, idx) => (
-                <SidebarLink key={idx} link={link} />
-              ))}
+    <>
+      {" "}
+      <div
+        className={cn(
+          "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 max-w-7xl border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+          "md:h-screen sm:h-8 md:sticky top-0 h-screen"
+        )}
+      >
+        <Sidebar open={open} setOpen={setOpen}>
+          <SidebarBody className="justify-between gap-10">
+            <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+              {open ? <Logo /> : <LogoIcon />}
+              <div className="mt-8 flex flex-col gap-2">
+                {navData.map((link, idx) => (
+                  <SidebarLink key={idx} link={link} />
+                ))}
+              </div>
             </div>
-          </div>
-          <div>
-            <SidebarLink
-              link={{
-                label: "Report a bug",
-                href: "#",
-                icon: (
-                  <IconBug className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-                ),
-              }}
-            />
-          </div>
-        </SidebarBody>
-      </Sidebar>
-    </div>
+            <div>
+              <SidebarLink
+                link={{
+                  label: "Logout",
+                  href: "#",
+                  icon: (
+                    <IconLogout2 className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+                  ),
+                  onClick: () => {
+                    setShowConfirmLogoutModal(true);
+                  },
+                }}
+              />
+
+              <SidebarLink
+                link={{
+                  label: "Report a bug",
+                  href: "#",
+                  icon: (
+                    <IconBug className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+                  ),
+                }}
+              />
+            </div>
+          </SidebarBody>
+        </Sidebar>
+      </div>
+      <ConfirmLogoutModal
+        show={showConfirmLogoutModal}
+        setShow={setShowConfirmLogoutModal}
+        showCats={true}
+        ctaBtnText="I promise to comeback soon, please give a virtual hug to cats and log me out!"
+        screenTitle="Wait! Before you go... 🐾"
+        screenDescription="We're just a bunch of humble cats, but we thought we'd ask... could you stay a little longer? We've got all these cozy corners and warm vibes, and honestly, it's just not the same without you here. But if you really must go, promise you'll come back soon? We'll be here, purring patiently and counting the seconds. 💖"
+        screenHeader="Request from cats and witches:"
+      />
+    </>
   );
 };
 
