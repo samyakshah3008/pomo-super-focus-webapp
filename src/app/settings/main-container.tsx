708adeb5c0d1c7071932eed9635ba7baf05aa4d3
Samyak Shah <samyakshah3008@gmail.com>
2024-10-30 11:34:42 +0530

    feat - settings flow containers add

--- src/app/settings/main-container.tsx
+++ src/app/settings/main-container.tsx
@@ -1,17 +1,62 @@
 "use client";
 
+import { Loader } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import BasicInformation from "./basic-information";
 import CredentialInformation from "./credential-information";
 import IntegrationsInformation from "./integrations-information";
+import LogoutFlow from "./logoutFlow";
 import Notifications from "./notifications";
 
-const MainContainer = () => {
+const MainContainer = ({
+  showConfirmLogoutModal,
+  setShowConfirmLogoutModal,
+  showExplodingHeart,
+  setShowExplodingHeart,
+}: any) => {
+  const [basicInformationObj, setBasicInformationObj] = useState({
+    firstName: "",
+    lastName: "",
+  });
+  const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const currentUser = useSelector((state: any) => state?.user?.pomoSuperUser);
+
+  useEffect(() => {
+    if (!currentUser?._id) return;
+    setBasicInformationObj({
+      firstName: currentUser?.firstName,
+      lastName: currentUser?.lastName,
+    });
+    setEmail(currentUser?.email);
+    setIsLoading(false);
+  }, [currentUser]);
+
+  if (isLoading) {
+    return (
+      <div className="h-96 flex items-center">
+        <Loader className="mr-2 h-8 w-8 animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col gap-4 w-[70%] m-auto ">
-      <BasicInformation />
-      <CredentialInformation />
+      <BasicInformation
+        basicInformationObj={basicInformationObj}
+        setBasicInformationObj={setBasicInformationObj}
+      />
+      <CredentialInformation email={email} setEmail={setEmail} />
       <IntegrationsInformation />
       <Notifications />
+      <LogoutFlow
+        showConfirmLogoutModal={showConfirmLogoutModal}
+        setShowConfirmLogoutModal={setShowConfirmLogoutModal}
+        showExplodingHeart={showExplodingHeart}
+        setShowExplodingHeart={setShowExplodingHeart}
+      />
     </div>
   );
 };
