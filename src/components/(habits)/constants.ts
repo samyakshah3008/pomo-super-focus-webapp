4afffc7f8aac077233de9b61073658ef68acc35e
Samyak Shah <samyakshah3008@gmail.com>
2024-11-18 22:23:13 +0530

    feat - constants add for habits scorecard creation flow

---
+++ src/components/(habits)/constants.ts
@@ -0,0 +1,34 @@
+const defineHabitsExamples = [
+  "Put up my running shoes",
+  "Take a deep breath",
+  "Meditate for 5mins",
+  "Eat one bite of spinach",
+  "Write one sentence",
+  "Text one friend",
+  "Go for a 10min walk",
+];
+
+const getSpecificExamples = [
+  "Every day at 8am",
+  "Every weekday lunch hour",
+  "After I eat breakfast",
+  "In the bathroom",
+  "When I wake up",
+  "When I close my work laptop",
+];
+
+const groundItToIdentityExamples = [
+  "An active person",
+  "A mindful person",
+  "A person who is wise with money",
+  "A dedicated musician",
+  "A gardener",
+  "A writer",
+  "A healthy person",
+];
+
+export {
+  defineHabitsExamples,
+  getSpecificExamples,
+  groundItToIdentityExamples,
+};
