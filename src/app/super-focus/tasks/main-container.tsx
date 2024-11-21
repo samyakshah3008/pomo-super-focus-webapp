89e9db43447ed4df0109d1d3326cdcf5f91f75ef
Samyak Shah <samyakshah3008@gmail.com>
2024-11-22 01:58:57 +0530

    feat - super focus tasks route and component add

---
+++ src/app/super-focus/tasks/main-container.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Lottie from "lottie-react";
+import Underconstruction from "../../../../public/under-construction-man.json";
+
+const MainContainer = () => {
+  return (
+    <div className="w-[70%] m-auto flex flex-col items-center gap-5 justify-center">
+      <div className="w-96 h-96">
+        <Lottie animationData={Underconstruction} loop={true} />
+      </div>
+      <div className="text-2xl font-bold text-center">
+        This feature is under construction, we will update you once we launch
+        this. Meanwhile, we appreciate your patience:)
+      </div>
+    </div>
+  );
+};
+
+export default MainContainer;
