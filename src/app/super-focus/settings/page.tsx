f5692480484603882fb489930f8ad2497f3c230c
Samyak Shah <samyakshah3008@gmail.com>
2024-11-22 22:52:03 +0530

    fix: made subtabs of super focus private

--- src/app/super-focus/settings/page.tsx
+++ src/app/super-focus/settings/page.tsx
@@ -1,5 +1,13 @@
+import { accessTokenKeyBrowserStorage } from "@/constants/browser-storage";
+import { getCookie } from "cookies-next";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import MainContainer from "./main-container";
 
 export default async function SuperFocusGuide() {
+  const accessToken = getCookie(accessTokenKeyBrowserStorage, { cookies });
+  if (!accessToken) {
+    redirect("/signin");
+  }
   return <MainContainer />;
 }
