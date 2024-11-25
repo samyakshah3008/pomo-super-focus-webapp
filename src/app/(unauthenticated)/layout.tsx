74c34d96c33725ed95f9ad9fa191351376a1fa7b
Samyak Shah <samyakshah3008@gmail.com>
2024-11-26 03:14:23 +0530

    audit fixes

--- src/app/(unauthenticated)/layout.tsx
+++ src/app/(unauthenticated)/layout.tsx
@@ -9,9 +9,9 @@ export default function UnauthenticatedLayout({
     <>
       <div className="fixed top-0 bg-yellow-300 w-full p-2 text-center text-sm rounded-bl-md rounded-br-md ">
         Please note that since we are running on initial plan for the
-        deployments, we cannot face heavy traffic for now. So please use Guest
-        Login to try our product, we promise to give you a verified account
-        access to the soonest!
+        deployments, Verified Account access is disabled temporarily. So please
+        use Guest Login to try our product, incase you need full access you can
+        request us for beta access at samyakshah3008@gmail.com
       </div>
       <main className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden px-4 py-12 md:p-12 lg:p-24">
         <div>
