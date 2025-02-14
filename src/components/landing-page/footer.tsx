cc40f675d11b23ccba428c29410214bd9c85dc19
Samyak Shah <samyakshah3008@gmail.com>
2025-02-14 18:50:20 +0530

    feat - landing page updates  - add awards section

--- src/components/landing-page/footer.tsx
+++ src/components/landing-page/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="mt-20 p-5 flex justify-center text-sm">
-      <div>© 2024 PomoSuperFocus. All rights reserved.</div>
+      <div>© 2025 PomoSuperFocus. All rights reserved.</div>
     </div>
   );
 };
